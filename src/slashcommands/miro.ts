fe3983bc691b7cafb6fc295a0a1e44e274c9e96e
myselfmohdsameer <msamerr57@gmail.com>
2023-06-08 20:48:35 +0530

    Setup app, user auth and persistence

---
+++ src/slashcommands/miro.ts
@@ -0,0 +1,90 @@
+import {
+    IHttp,
+    IModify,
+    IPersistence,
+    IRead,
+} from "@rocket.chat/apps-engine/definition/accessors";
+import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
+import {
+    ISlashCommand,
+    SlashCommandContext,
+} from "@rocket.chat/apps-engine/definition/slashcommands";
+import { IUser } from "@rocket.chat/apps-engine/definition/users";
+import { MiroApp } from "../../MiroApp";
+import { Subcommands } from "../enums/Subcommands";
+import { Texts } from "../enums/Texts";
+import { sendNotification } from "../lib/message";
+import { authorize } from "./subcommands/authorize";
+
+export class Miro implements ISlashCommand {
+    public command = "miro-app";
+    public i18nParamsExample = "slashcommand_params";
+    public i18nDescription = "slashcommand_description";
+    public providesPreview = false;
+
+    constructor(private readonly app: MiroApp) {}
+
+    public async executor(
+        context: SlashCommandContext,
+        read: IRead,
+        modify: IModify,
+        http: IHttp,
+        persistence: IPersistence
+    ): Promise<void> {
+        const command = this.getCommandFromContextArguments(context);
+        if (!command) {
+            return await this.displayAppHelpMessage(
+                read,
+                modify,
+                context.getSender(),
+                context.getRoom()
+            );
+        }
+
+        switch (command) {
+            case Subcommands.Help:
+                await this.displayAppHelpMessage(
+                    read,
+                    modify,
+                    context.getSender(),
+                    context.getRoom()
+                );
+                break;
+            case Subcommands.Auth:
+                await authorize(
+                    this.app,
+                    read,
+                    modify,
+                    context.getSender(),
+                    persistence
+                );
+                break;
+            default:
+                await this.displayAppHelpMessage(
+                    read,
+                    modify,
+                    context.getSender(),
+                    context.getRoom()
+                );
+                break;
+        }
+    }
+
+    private getCommandFromContextArguments(
+        context: SlashCommandContext
+    ): string {
+        const [command] = context.getArguments();
+        return command;
+    }
+
+    private async displayAppHelpMessage(
+        read: IRead,
+        modify: IModify,
+        user: IUser,
+        room: IRoom
+    ): Promise<void> {
+        const text = Texts.Help;
+
+        return sendNotification(read, modify, user, room, text);
+    }
+}
---
+++ src/storage/users.ts
@@ -0,0 +1,198 @@
+import {
+    IPersistence,
+    IPersistenceRead,
+    IRead,
+} from "@rocket.chat/apps-engine/definition/accessors";
+import {
+    RocketChatAssociationModel,
+    RocketChatAssociationRecord,
+} from "@rocket.chat/apps-engine/definition/metadata";
+import {
+    IAuthData,
+    IOAuth2ClientOptions,
+} from "@rocket.chat/apps-engine/definition/oauth2/IOAuth2";
+import { IUser } from "@rocket.chat/apps-engine/definition/users";
+
+export interface UserModel {
+    rocketChatUserId: string;
+    miroUserId: string;
+}
+
+const assoc = new RocketChatAssociationRecord(
+    RocketChatAssociationModel.MISC,
+    "users"
+);
+
+export const persistUserAsync = async (
+    persis: IPersistence,
+    rocketChatUserId: string,
+    miroUserId: string
+): Promise<void> => {
+    const associationsByRocketChatUserId: Array<RocketChatAssociationRecord> = [
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.MISC,
+            "User"
+        ),
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.USER,
+            rocketChatUserId
+        ),
+    ];
+    const associationsByTeamsUserId: Array<RocketChatAssociationRecord> = [
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.MISC,
+            "User"
+        ),
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.USER,
+            miroUserId
+        ),
+    ];
+    const data: UserModel = {
+        rocketChatUserId,
+        miroUserId,
+    };
+
+    await persis.updateByAssociations(
+        associationsByRocketChatUserId,
+        data,
+        true
+    );
+    await persis.updateByAssociations(associationsByTeamsUserId, data, true);
+};
+
+export const retrieveUserByRocketChatUserIdAsync = async (
+    read: IRead,
+    rocketChatUserId: string
+): Promise<UserModel | null> => {
+    const associations: Array<RocketChatAssociationRecord> = [
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.MISC,
+            "User"
+        ),
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.USER,
+            rocketChatUserId
+        ),
+    ];
+
+    const persistenceRead: IPersistenceRead = read.getPersistenceReader();
+    const results = await persistenceRead.readByAssociations(associations);
+
+    if (results === undefined || results === null || results.length == 0) {
+        return null;
+    }
+
+    if (results.length > 1) {
+        throw new Error(
+            `More than one User record for user ${rocketChatUserId}`
+        );
+    }
+
+    const data: UserModel = results[0] as UserModel;
+    return data;
+};
+
+export const retrieveUserBymiroUserIdAsync = async (
+    read: IRead,
+    miroUserId: string
+): Promise<UserModel | null> => {
+    const associations: Array<RocketChatAssociationRecord> = [
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.MISC,
+            "User"
+        ),
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.USER,
+            miroUserId
+        ),
+    ];
+
+    const persistenceRead: IPersistenceRead = read.getPersistenceReader();
+    const results = await persistenceRead.readByAssociations(associations);
+
+    if (results === undefined || results === null || results.length == 0) {
+        return null;
+    }
+
+    if (results.length > 1) {
+        throw new Error(`More than one User record for user ${miroUserId}`);
+    }
+
+    const data: UserModel = results[0] as UserModel;
+    return data;
+};
+
+export async function create(
+    read: IRead,
+    persistence: IPersistence,
+    user: IUser
+): Promise<void> {
+    const users = await getAllUsers(read);
+
+    if (!users) {
+        await persistence.createWithAssociation([user], assoc);
+        return;
+    }
+
+    if (!isUserPresent(users, user)) {
+        users.push(user);
+        await persistence.updateByAssociation(assoc, users);
+    }
+}
+
+export async function remove(
+    read: IRead,
+    persistence: IPersistence,
+    user: IUser
+): Promise<void> {
+    const users = await getAllUsers(read);
+
+    if (!users || !isUserPresent(users, user)) {
+        // @NOTE do nothing
+        return;
+    }
+
+    const idx = users.findIndex((u: IUser) => u.id === user.id);
+    users.splice(idx, 1);
+    await persistence.updateByAssociation(assoc, users);
+}
+
+export async function getAllUsers(read: IRead): Promise<IUser[]> {
+    const data = await read.getPersistenceReader().readByAssociation(assoc);
+    return data.length ? (data[0] as IUser[]) : [];
+}
+
+function isUserPresent(users: IUser[], targetUser: IUser): boolean {
+    return users.some((user) => user.id === targetUser.id);
+}
+
+/**
+ * This function needed to be copied from the apps engine due to difficulties trying to
+ * get access to the auth client from inside a job processor.
+ * @NOTE It relies on hardcoded information (config alias's suffix) to work and it might break if
+ * the value changes
+ */
+export async function getAccessTokenForUser(
+    read: IRead,
+    user: IUser
+): Promise<IAuthData | undefined> {
+    const associations = [
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.USER,
+            user.id
+        ),
+        new RocketChatAssociationRecord(
+            RocketChatAssociationModel.MISC,
+            `miro-app-oauth-connection`
+        ),
+    ];
+
+    const [result] = (await read
+        .getPersistenceReader()
+        .readByAssociations(associations)) as unknown as Array<
+        IAuthData | undefined
+    >;
+
+    return result;
+}
